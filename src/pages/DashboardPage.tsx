370a9ba49bd05946f1b9cbd84de79e53d68c8bc2
Lil-Greg <gregorywilliams1289@gmail.com>
2024-05-23 07:44:13 -0400

    Basic Start

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,14 +1,21 @@
 import { useContext } from "react"
 import UserContext from "../context/UserContext"
+import getImageURL from "../utils/image-util";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 export default function DashboardPage() {
     const { user } = useContext(UserContext);
     return (
         <>
-            <div className="container">
-                {user && user.type && user.username}
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        {user && user.type && user.username}
+                        <img src={getImageURL("default-pfp.jpg")} alt="User Profile Picture" />
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
