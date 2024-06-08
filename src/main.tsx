63906ece466d25287113a83f257082977cff713c
Landing-s <landings.35@gmail.com>
2024-06-08 11:37:45 -0400

    changed route path

--- src/main.tsx
+++ src/main.tsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
     path: '/register',
     element: <RegisterPage/> //<ProtectedRoute allowedUserTypes={['administrator', 'super']}><RegisterPage /></ProtectedRoute>
   },{
-    path:'/login/forgot',
+    path:'/forgot',
     element:<ForgotPassword/>
   },
 ]);
