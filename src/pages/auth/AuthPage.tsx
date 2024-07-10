d49fe986cf3787833eeb63be331b84ea3f391599
Roboitey <Islerih25@gmail.com>
2024-07-09 20:32:40 -0400

    Captcha/Login Attempts not working

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,6 +1,5 @@
 import './AuthPage.css';
-import { FaRegEyeSlash } from "react-icons/fa6";
-import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useContext, useRef, useState } from "react";
@@ -8,69 +7,86 @@ import { NavLink, useNavigate } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import useAuth from "../../hooks/useAuth";
 import { Button, Col, Container, InputGroup, Row } from 'react-bootstrap';
-import { useMutation, useQuery } from 'convex/react';
+import { useQuery } from 'convex/react';
 import { api } from '../../../convex/_generated/api';
-import GetIp from '../../hooks/getIp';
+import getImageURL from '../../utils/image-util';
 
 export default function AuthPage() {
     const getAllUsers = useQuery(api.users.get);
-    const { userIp } = GetIp();
-    const setIpAndLogin = useMutation(api.users.setIpAndRecentLogin);
     const { setUser } = useContext(UserContext);
     const navigate = useNavigate();
     const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    const captchaRef = useRef<HTMLInputElement>(null);
     const [passwordShow, setPasswordShow] = useState(false);
     const [invalidUser, setInvalidUser] = useState(false);
+    const [invalidCaptcha, setInvalidCaptcha] = useState(false);
+    const { success, setParams, user } = useAuth();
+    const [loginAttemptsRemaining, setLoginAttemptsRemaining] = useState(3);
+
+    const correctCaptcha = "12uI9045"; 
 
     const handleSubmit = (event: React.FormEvent) => {
-        // event.preventDeault will prevent the page to refresh after the alert
         event.preventDefault();
         const username = usernameRef.current?.value;
         const password = passwordRef.current?.value;
+        const captcha = captchaRef.current?.value;
 
         const checkUsername = getAllUsers?.filter(userData => userData.username === username);
         const checkPassword = getAllUsers?.filter(userData => userData.password === password);
 
-        if (!userIp) {
-            throw Error("User's IP is Undefined");
-        };
-        if (checkUsername) {
-            setIpAndLogin({ id: checkUsername[0]._id, ip: userIp });
-        };
-        if (username && password) {
-            if (checkPassword && checkPassword.length > 0) {
-                setInvalidUser(false);
-                setUser && setUser(checkPassword[0]); // Set Types
-                window.localStorage.setItem('election-user', JSON.stringify(checkPassword[0]));
-                navigate('/', { replace: true });
-            } else if (checkUsername?.length === 0 || checkPassword?.length === 0) {
-                setInvalidUser(true);
+        if (username && password && captcha && loginAttemptsRemaining > 0) {
+            if (captcha === correctCaptcha) {
+                setInvalidCaptcha(false);
+                setParams({
+                    username: username,
+                    password: password
+                });
+
+                if (success) {
+                    setInvalidUser(false);
+                    setUser && setUser(user);
+                    window.localStorage.setItem('election-user', JSON.stringify(user));
+                    navigate('/', { replace: true });
+                } else if (!success && (checkUsername?.length === 0 || checkPassword?.length === 0)) {
+                    setInvalidUser(true);
+                    setLoginAttemptsRemaining(loginAttemptsRemaining - 1);
+                }
+            } else {
+                setInvalidCaptcha(true);
             }
         } else {
-            alert(`Please insert value`);
+            alert(`Please fill in all fields`);
         }
-    }
+    };
+
     const togglePasswordShow = () => {
         setPasswordShow(!passwordShow);
-    }
-    return <>
+    };
+
+    return (
         <Container className="mt-3" style={{ textAlign: 'center' }}>
             <h1>Login</h1>
             {invalidUser && (
                 <p style={{ color: '#dc3545', display: 'inline' }}>User Does Not Exist</p>
             )}
+            {invalidCaptcha && (
+                <p style={{ color: '#dc3545', display: 'inline' }}>Incorrect Captcha. Please try again.</p>
+            )}
+            {loginAttemptsRemaining <= 0 && (
+                <p style={{ color: '#dc3545', display: 'inline' }}> You Have Been Locked Out For 1 Hour</p>
+            )}
             <Form
                 style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
                 onSubmit={handleSubmit}
-                className={invalidUser ? 'login-user-invalid' : ''}
+                className={invalidUser || loginAttemptsRemaining <= 0 ? 'login-user-invalid' : ''}
             >
                 <FloatingLabel
                     controlId="floatingInputUsername"
                     label="Username"
                     className='mb-3 w-50'
                 >
-                    <Form.Control autoComplete="off" type="text" ref={usernameRef} placeholder="Username" />
+                    <Form.Control autoComplete="off" type="text" ref={usernameRef} placeholder="Username" className={invalidUser ? 'is-invalid' : ''} />
                 </FloatingLabel>
 
                 <InputGroup className="mb-1 w-50">
@@ -78,18 +94,32 @@ export default function AuthPage() {
                         controlId="floatingInputPassword"
                         label="Password"
                     >
-                        <Form.Control type={passwordShow ? "text" : "password"} placeholder="Password" ref={passwordRef} />
+                        <Form.Control type={passwordShow ? "text" : "password"} placeholder="Password" ref={passwordRef} className={invalidUser ? 'is-invalid' : ''} />
                     </FloatingLabel>
                     <InputGroup.Text onClick={togglePasswordShow}>{passwordShow ? <FaRegEyeSlash /> : <FaRegEye />}</InputGroup.Text>
                 </InputGroup>
-                <Row>
 
+                <InputGroup className="mb-3 w-50">
+                    <FloatingLabel
+                        controlId="floatingInputCaptcha"
+                        label="Captcha"
+                    >
+                        <Form.Control type="text" placeholder="Enter Captcha" ref={captchaRef} className={invalidCaptcha ? 'is-invalid' : ''} />
+                    </FloatingLabel>
+                    <span>&nbsp;&nbsp;&nbsp;</span>
+                    <img src={getImageURL("Captcha.png")} alt="captcha" className="captcha-image" />
+                </InputGroup>
+
+                <Row>
                     <a className="m-1" style={{ textDecoration: 'none' }} href="/forgot"><h5>Forgot Password?</h5></a>
                 </Row>
                 <Row>
                     <NavLink to={'/register'}><h5>Sign Up</h5></NavLink>
                 </Row>
                 <Row className="mt-2 mb-4">
+                    <Col xs="auto">
+                        {loginAttemptsRemaining} Login Attempts Remaining
+                    </Col>
                     <Col xs="auto">
                         <Button variant="primary" type="submit">
                             Submit
@@ -107,5 +137,5 @@ export default function AuthPage() {
                 </Row>
             </Form>
         </Container>
-    </>
+    );
 }
